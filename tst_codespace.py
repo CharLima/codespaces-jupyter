52074828ae478fbaf448dd7a8e2b10a5e8fc1bf7
Charles de Lima Soares <76947358+CharLima@users.noreply.github.com>
2023-02-23 04:10:45 +0000

    Agora vamos adicionar mais coisas

--- tst_codespace.py
+++ tst_codespace.py
@@ -1,3 +1,19 @@
 if __name__ == '__main__':
     print("Hello World!!")
     
+    #### Agora vamos adicionar mais coisas ####
+
+        #### Madlibs ####
+    from random import randint as rr
+
+
+    numero = rr(0, 6)
+    num = input("Adivinhe o número (de 1 a 6): ")
+    madlib = f"Você escolheu o número: {num}\n" \
+             f"E o número sorteado é: {numero}."
+    print(madlib)
+    if num == numero:
+        print("Parabéns!!")
+    else:
+        print("Você perdeu.")
+    
